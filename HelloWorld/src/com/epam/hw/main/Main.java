95fac6ea9b6d903ed55e21d5bb00c9fb6897f214
AndreiBordak <Andrei_Bordak@epam.com>
2015-03-19 23:45:24 +0300

    HelloWorld
    hw

---
+++ HelloWorld/src/com/epam/hw/main/Main.java
@@ -0,0 +1,9 @@
+package com.epam.hw.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String author = System.getProperty("author");
+		System.out.println("Hello word, it's " + author + "!");
+	}
+}
